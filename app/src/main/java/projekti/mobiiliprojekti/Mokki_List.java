d7a6eba10e6f76a2a6c6d3ab4986060431114f8f
Ville Kovalainen <lolwest@suomi24.fi>
2021-03-23 11:14:38 +0200

    draweri ja custom actionbar päänäkymässä

--- app/src/main/java/projekti/mobiiliprojekti/Mokki_List.java
+++ app/src/main/java/projekti/mobiiliprojekti/Mokki_List.java
@@ -1,15 +1,21 @@
 package projekti.mobiiliprojekti;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 
 public class Mokki_List extends AppCompatActivity {
+
+
+    DrawerLayout drawerLayout;
 
     private ArrayList<MokkiItem> mMokkiItem;
 
@@ -21,6 +27,7 @@ public class Mokki_List extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mokki__list);
+        drawerLayout = findViewById(R.id.drawer_layout);
 
         createMokkiItem();
         buildRecyclerView();
@@ -82,5 +89,19 @@ public class Mokki_List extends AppCompatActivity {
                 startActivity(intentMokkiNakyma);
             }
         });
+    }
+
+    public void openMenu(View view) {
+        openDrawer(drawerLayout);
+    }
+
+    public void closeMenu(View view) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    private static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
     }
 }
